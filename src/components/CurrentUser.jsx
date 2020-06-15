a57185d7b1557db671543494cbdb797db4c6dd7a
oluwadareseyi <oluwadareseyii@gmail.com>
2020-06-15 12:15:28 +0100

    export signOut as separate function

--- src/components/CurrentUser.jsx
+++ src/components/CurrentUser.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import moment from "moment";
 
-import { auth } from "../firebase";
+import { signOut } from "../firebase";
 
 const CurrentUser = ({ displayName, photoURL, email, createdAt, children }) => {
   return (
@@ -17,7 +17,7 @@ const CurrentUser = ({ displayName, photoURL, email, createdAt, children }) => {
       </div>
       <div>
         <div>{children}</div>
-        <button onClick={() => auth.signOut()}>Sign Out</button>
+        <button onClick={signOut}>Sign Out</button>
       </div>
     </section>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -20,6 +20,8 @@ export const provider = new firebase.auth.GoogleAuthProvider();
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
+export const signOut = () => auth.signOut();
+
 window.firebase = firebase;
 
 export default firebase;
